07817efad0ff1f7c487c9e414e6c9f5d845debc8
yelsindatu <yelsin24@mhs.unitama.ac.id>
2026-06-15 00:10:42 -0700

    feat: implementasi database transaction pada update dokter

--- resources/views/dokter/edit.blade.php
+++ resources/views/dokter/edit.blade.php
@@ -72,6 +72,19 @@
             @enderror
         </div>
 
+        <div class="mb-3">
+            <label class="form-label">Tanggal</label>
+
+            <input type="date" name="tanggal" class="form-control @error('tanggal') is-invalid @enderror"
+                value="{{ old('tanggal', $dokter->tanggal) }}">
+
+            @error('tanggal')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
         <a href="{{ route('dokter.index') }}" class="btn btn-warning">
             Cancel
         </a>
